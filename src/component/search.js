22a3669d5e357d8beff86ca1beecc00c6e5126dc
Ambica <ambicana5@gmail.com>
2022-10-19 16:08:48 +0545

    added search component

--- src/component/search.js
+++ src/component/search.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import {useState, useEffect} from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass,faHeart, faCartShopping} from '@fortawesome/free-solid-svg-icons'
+import { increment } from '../features/counter/counter.slice';
+import { useDispatch} from 'react-redux';
 
 const Search = ()=>{
+	const dispatch = useDispatch();
+
 	const [searchItem, setSearchItem] = useState(null)
 	const [products, setProducts] = useState([])
 	const fetchList = () => {
@@ -16,12 +20,26 @@ const Search = ()=>{
 		fetchList()
 	},[])
 
+	const onUpClick = async(id,isLiked) => {
+		dispatch(increment());
+		
+		// console.log(id)
+		const requestOptions = {
+			method: 'PUT',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({isLiked: isLiked ? false : true})
+		  };
+		  const response = await fetch('http://localhost:3001/products/' + id, requestOptions);
+		  const data = await response.json();
+		  if(data){
+			fetchList()
+		  }
+	};
+
 	const searchProduct = (value)=>{
 		const newProducts = [...products]
 		const filterProduct = newProducts.filter((item)=>{
-			if(searchItem.include()){
-
-			}
+			return item.category === value
 		})
 		setProducts(filterProduct)
 	}
@@ -33,6 +51,8 @@ const Search = ()=>{
 					<FontAwesomeIcon icon={faMagnifyingGlass} />
 				</button>
 			</div>
+
+			
 		</>
 	)
 }
