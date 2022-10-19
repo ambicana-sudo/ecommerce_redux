264979d792e025193d1f995da4035b21552d8d1e
Ambica <ambicana5@gmail.com>
2022-10-19 09:19:28 +0545

    added new route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Register from "./container/forms/register"
 import ErrorPage from "./container/error-page"
 import AddProduct from "./container/forms/add-product";
 import Home from "./container/home";
+import Wishlist from "./container/wishlist";
+import Cart from "./container/cart";
 // import {useState, useEffect} from 'react'
 
 import {
@@ -12,6 +14,7 @@ import {
 	Routes,
 	Route,
  } from "react-router-dom";
+import { integerPropType } from "@mui/utils";
 
 const App = ()=> {
 
@@ -24,6 +27,8 @@ const App = ()=> {
 				<Route path="/register" element={<Register/>}></Route>
 				<Route path="/admin" element={<AddProduct/>}></Route>
 				<Route path="/home" element={<Home/>}></Route>
+				<Route path="/wishlist" element={<Wishlist/>}></Route>
+				<Route path="/cart" element={<Cart/>}></Route>
 				<Route element={<ErrorPage/>}></Route>
 			</Routes>
 		</Router>
