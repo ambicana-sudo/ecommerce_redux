3458e26c88657f17bbeb49eb67ffafbe4088a12d
Ambica <ambicana5@gmail.com>
2022-10-20 19:48:22 +0545

    added wishlist page style

--- src/container/wishlist.js
+++ src/container/wishlist.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faXmark } from '@fortawesome/free-solid-svg-icons'
+import { faXmark, faCartShopping } from '@fortawesome/free-solid-svg-icons'
 import Image from '../images/default-thumbnail.jpg'
 import { increment, decrement } from '../features/counter/counter.slice';
 import { useDispatch, useSelector} from 'react-redux';
@@ -34,37 +34,60 @@ const Wishlist = ()=>{
     return(
 		<section className='wishlist-section'>
 			<div className='container'>
-				<h1>My Wishlist</h1>
+				<div className='page-heading'>
+					<h1>My Wishlist</h1>
+				</div>
+				
 				<div className="product wished-product">
-					{products.map((item)=>{
-						const {name, category, price} = item;
-						if(item.isLiked === true){
-							return(
-								<div className="product-list">
-									<div className="product-image">
-										<img src={Image} alt=""/>
-									</div>
-
-									<div className="product-info">
-										<h3 className="product-name">{name}</h3>
-										<p className="product-price"><em>${price}</em></p>
-									</div>
-
-									<div className='quantity'>
-										<button onClick={()=> onUpClick()}>+</button>
-										<p>{count}</p>
-										<button onClick={()=>onDownClick()}>-</button>
-									</div>
-					
-									<div className="product-btn">
-										<button className="delete">
-											<FontAwesomeIcon icon={faXmark} />
-										</button>
-									</div>
-								</div>
-							)
-						}
-					})}
+					<table>
+						<thead>
+							<tr>
+								<th></th>
+								<th>Product</th>
+								<th>Quantity</th>
+								<th>Price</th>
+								<th>Add to cart</th>
+								<th>Delete</th>
+							</tr>
+						</thead>
+						<tbody>
+							
+								{products.map((item)=>{
+									const {name, category, price} = item;
+									if(item.isLiked === true){
+										return(
+											<tr>
+												<td>
+														<img src={Image} alt=""/>
+												</td>
+												<td>
+														<h3 className="product-name">{name}</h3>
+												</td>
+												<td><p className="product-price"><em>${price}</em></p></td>
+												<td>
+													<div className='quantity'>
+														<button onClick={()=> onUpClick()}>+</button>
+														<p className='num'>{count}</p>
+														<button onClick={()=>onDownClick()}>-</button>
+													</div>
+												</td>
+												<td>
+													<button className="cart">
+														<FontAwesomeIcon icon={faCartShopping} />
+													</button>
+												</td>
+												<td>
+													<button className="delete">
+														<FontAwesomeIcon icon={faXmark} />
+													</button>
+												</td>
+											</tr>
+										)
+									}
+								})}
+							
+						</tbody>
+					</table>
 				</div>
 			</div>
 		</section>
