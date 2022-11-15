7b378c22155e5b1294584b53d544eb0e4fa614f7
Ambica <ambicana5@gmail.com>
2022-11-15 23:27:12 +0545

    added products to cart using redux

---
+++ src/redux/product/productItem.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { productData, selectedProduct } from './productItem.slice'
+import { useDispatch, useSelector } from 'react-redux'
+
+const ProductListing = ()=>{
+    const test = useSelector(state=>state.product)
+    const dispatch = useDispatch()
+    console.log(test)
+
+    // const getProduct = ()=> {
+    //     dispatch(productData())
+    // }
+
+    return(
+        <></>
+    )
+}
+export default ProductListing
